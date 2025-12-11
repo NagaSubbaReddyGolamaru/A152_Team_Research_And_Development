cb23a1df3503e4b94dd99cdbcaef78d3dfd01a9e
NagaSubbaReddyGolamaru <ng25aaq@herts.ac.uk>
2025-12-11 12:36:44 +0530

    Commit 2: Added Shapiro-Wilk normality tests

--- step7_salary_analysis.R
+++ step7_salary_analysis.R
@@ -10,3 +10,25 @@ female_salaries <- data_clean %>%
 
 cat("\n=== ASSUMPTION CHECKS ===\n")
 cat("\nNormality Tests (Shapiro-Wilk):\n")
+
+# Normality Tests (Shapiro-Wilk)
+
+if (length(male_salaries) > 500) {
+  set.seed(123)
+  male_sample <- sample(male_salaries, 500)
+  shapiro_male <- shapiro.test(male_sample)
+} else {
+  shapiro_male <- shapiro.test(male_salaries)
+}
+
+if (length(female_salaries) > 500) {
+  set.seed(123)
+  female_sample <- sample(female_salaries, 500)
+  shapiro_female <- shapiro.test(female_sample)
+} else {
+  shapiro_female <- shapiro.test(female_salaries)
+}
+
+cat("\n** INTERPRETATION OF NORMALITY TESTS **")
+cat("\nBoth p-values < 0.05 indicate departure from normality.")
+cat("\nThis suggests using non-parametric tests as a robustness check.\n")
